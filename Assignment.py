73bc582d82e554af3f2b2f595601f4ec26fe2a00
Rajath Krishna M <rkcutz2345@gmail.com>
2025-10-17 22:36:20 +0530

    Delete Assignment.py

--- Assignment.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter 1st number: "))
-b=int(input("Enter 2nd number: "))
-
-print("Sum of the two numbers:",a+b)
-print("Difference:",a-b)
-print("Product of two numbers:",a*b)
-print("Division:",a/b)
